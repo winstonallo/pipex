2cf1c40e834586b1afd3c808a51641a450e1cf7f
Arthur <abied-ch@c2r4p3.42vienna.com>
2023-10-01 23:33:54 +0200

    hey

--- src/pipex.c
+++ src/pipex.c
@@ -6,27 +6,14 @@
 /*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 20:26:50 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/09/29 21:39:56 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/10/01 22:55:07 by abied-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-int	main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv)
 {
-	int		fd_input;
-	int		fd_output;
-	char	*str;
 	
-	while (argc)
-		break ;
-	fd_input = open(argv[1], O_RDONLY);
-	dup2(fd_input, 0);
-	str = get_next_line(fd_input);
-	fd_output = open(argv[4], O_WRONLY);
-	dup2(fd_output, 1);
-	execve(str, argv, envp);
-	close(fd_input);
-	close(fd_output);
 	return (0);
 }
