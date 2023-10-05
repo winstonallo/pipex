388df260a9cef37617218ed991ec29a5be7d1f84
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2023-10-06 01:31:11 +0200

    Update and rename children.c to family.c

--- src/family.c
+++ src/family.c
@@ -12,23 +12,51 @@
 
 #include "../include/pipex.h"
 
-void	give_birth1(t_list *data, char **envp)
+void	fuck_mich_nich_ab_jz(char *s, char **a, char **e, t_list *d)
 {
-	if (dup2(data->input_fd, STDIN_FILENO) == -1)
-		perror("DUP2 child 1");
-	dup2(data->pipe[1], STDOUT_FILENO);
-	close(data->pipe[0]);
-	if (execve(data->input_path, data->input_args, envp) == -1)
-		perror("EXECVE child 1");		
+	if (execve(s, a, e) == -1)
+	{
+		perror("execve");
+		cleanup(d);
+		exit(0);
+	}
 }
 
-void	parent(t_list *data, char **envp)
+void	firstborn(t_list *data, char **envp, char **argv)
 {
+	open_files(argv[4], 0, data);
+	if (dup2(data->input_fd, STDIN_FILENO) == -1)
+	{
+		perror("firstborn dup2");
+		cleanup(data);
+		exit(EXIT_FAILURE);
+	}
+	if (dup2(data->pipe[1], STDOUT_FILENO) == -1)
+	{
+		perror("firstborn dup2");
+		cleanup(data);
+		exit(EXIT_FAILURE);
+	}
+	close(data->pipe[0]);
+	fuck_mich_nich_ab_jz(data->input_path, data->input_args, envp, data);
+}
+
+void	mommy(t_list *data, char **envp, char **argv)
+{
+	open_files(argv[4], 1, data);
 	if (dup2(data->output_fd, STDOUT_FILENO) == -1)
-		perror("DUP2 child 2");
-	dup2(data->pipe[0], 0);
+	{
+		perror("mommy dup2");
+		cleanup(data);
+		exit(EXIT_FAILURE);
+	}
+	if (dup2(data->pipe[0], STDIN_FILENO) == -1)
+	{
+		perror("mommy dup2");
+		cleanup(data);
+		exit(EXIT_FAILURE);
+	}
 	close(data->pipe[1]);
-	if (execve(data->output_path, data->output_args, envp) == -1)
-		perror("EXECVE child 2");
+	fuck_mich_nich_ab_jz(data->output_path, data->output_args, envp, data);
 }
 
